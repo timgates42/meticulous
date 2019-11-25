08937240d4c8326d84ea1cb02d989660fdba804c
Tim Gates <tim.gates@iress.com>
2019-11-25 21:13:35 +1100

    Add test start

--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -3,17 +3,25 @@ Test modules for:
 
 meticulous.__main__
 """
+from __future__ import absolute_import, division, print_function
 
 from meticulous.__main__ import main
 
+import pytest
 
-def test_main():
+
+@pytest.mark.parametrize("args,", [(), ("check",)])
+def test_main(args):
     """
     GIVEN the .__main__ module entry point WHEN calling main THEN the call
     executes successfully with a result of `None`
     """
     # Setup
+    from click.testing import CliRunner
+
+    runner = CliRunner()
     # Exercise
-    result = main()  # pylint: disable=assignment-from-no-return
+    fullargs = list(args)
+    result = runner.invoke(main, fullargs)
     # Verify
-    assert result is None  # nosec
+    assert result.output == "X\n"  # noqa: S101 # nosec
