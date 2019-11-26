179fc9ad7762124b29ce5e7cc0bcb4c9bd723f4b
Tim Gates <tim.gates@iress.com>
2019-11-26 15:50:49 +1100

    Drop older python support

--- app/setup.py
+++ app/setup.py
@@ -64,7 +64,7 @@ setup(
     .strip(),
     long_description=load_include("README.md", transform=True),
     long_description_content_type="text/markdown",
-    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
+    python_requires=">=3.6",
     test_suite="tests",
     test_requires=[
         elem.strip()
@@ -82,10 +82,7 @@ setup(
         for elem in [
             "Development Status :: 4 - Beta",
             "Programming Language :: Python",
-            "Programming Language :: Python :: 2",
-            "Programming Language :: Python :: 2.7",
             "Programming Language :: Python :: 3",
-            "Programming Language :: Python :: 3.5",
             "Programming Language :: Python :: 3.6",
             "Programming Language :: Python :: 3.7",
             "Programming Language :: Python :: 3.8",
