093e270ba988947dcf6e73480d7ffe40f5912137
Tim Gates <tim.gates@iress.com>
2020-02-25 21:24:25 +1100

    Testing

--- app/meticulous/_websearch.py
+++ app/meticulous/_websearch.py
@@ -2,31 +2,44 @@
 Use the internet to determine if the provided word is a nonword or a typo
 """
 
-from urllib.parse import quote, unquote
 import re
-
-from bs4 import BeautifulSoup
+from urllib.parse import quote, unquote
 
 import requests
+from bs4 import BeautifulSoup
+
 
 class Suggestion:
-    def __init__(self, is_nonword=False, is_typo=False, replacement=''):
+    def __init__(self, is_nonword=False, is_typo=False, replacement=""):
         self.is_nonword = is_nonword
         self.is_typo = is_typo
         self.replacement = replacement
 
+    def __eq__(self, other):
+        """
+        Check equality
+        """
+        return (
+            self.is_nonword == getattr(other, "is_nonword", None)
+            and self.is_typo == getattr(other, "is_typo", None)
+            and self.replacement == getattr(other, "replacement", None)
+        )
+
+
 DICTIONARIES = [
-"https://www.merriam-webster.com/dictionary/",
-"https://en.wikipedia.org/wiki/",
-"https://www.dictionary.com/browse/",
-"https://en.wiktionary.org/wiki/",
-"https://www.collinsdictionary.com/dictionary/english/",
-"https://www.teachingenglish.org.uk/article/",
-"https://www.vocabulary.com/dictionary/",
-"https://www.thefreedictionary.com/",
-"https://www.thesaurus.com/browse/",
-"https://www.yourdictionary.com/",
+    "https://www.merriam-webster.com/dictionary/",
+    "https://en.wikipedia.org/wiki/",
+    "https://www.dictionary.com/browse/",
+    "https://en.wiktionary.org/wiki/",
+    "https://www.collinsdictionary.com/dictionary/english/",
+    "https://www.teachingenglish.org.uk/article/",
+    "https://www.vocabulary.com/dictionary/",
+    "https://www.thefreedictionary.com/",
+    "https://www.thesaurus.com/browse/",
+    "https://www.yourdictionary.com/",
 ]
+
+
 def get_suggestion(word):
     """
     Use the internet to determine if the provided word is a nonword or a typo
@@ -35,7 +48,7 @@ def get_suggestion(word):
     search_re = re.compile("[/]url[?]q=([^&#]+)[&#]")
     page = requests.get(search).text
     soup = BeautifulSoup(page, features="lxml")
-    for link in soup.find_all('a'):
+    for link in soup.find_all("a"):
         href = link.attrs.get("href")
         if not href:
             continue
---
+++ app/meticulous/_websearch_test.py
@@ -0,0 +1,20 @@
+"""
+Test cases for search results
+"""
+
+import pytest
+
+from meticulous._websearch import Suggestion, get_suggestion
+
+
+@pytest.mark.parametrize(
+    "word, expected", [("catenate", Suggestion(is_nonword=True))],
+)
+def test_suggestions(word, expected):
+    """
+    Test to ensure expected suggestions are obtained
+    """
+    # Exercise
+    obtained = get_suggestion(word)
+    # Verify
+    assert obtained == expected
