458d1c9faf6ecb61c010996d10a4b81637962e46
Tim Gates <tim.gates@iress.com>
2020-02-26 13:04:47 +1100

    black

--- app/meticulous/_websearch_test.py
+++ app/meticulous/_websearch_test.py
@@ -8,7 +8,8 @@ from meticulous._websearch import Suggestion, get_suggestion
 
 
 @pytest.mark.parametrize(
-    "word, expected", [
+    "word, expected",
+    [
         ("catenate", Suggestion(is_nonword=True)),
         ("actuall", Suggestion(is_typo=True)),
         ("altnernatives", Suggestion(is_typo=True, replacement="alternatives")),
