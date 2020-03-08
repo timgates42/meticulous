48f8acfb97714a8f11e63d3d181313063c7aa7bf
Tim Gates <tim.gates@iress.com>
2020-03-08 11:59:41 +1100

    Add fast pr support

--- app/meticulous/_summary.py
+++ app/meticulous/_summary.py
@@ -46,6 +46,27 @@ def display_repo_intro(path):
             display_summary(fpath)
 
 
+def display_and_check_files(path):
+    """
+    Scan and display files looking for the word or word start "expect"
+    """
+    regex = re.compile("expect", re.I)
+    suggest_issue = False
+    if path.is_dir():
+        for fpath in path.iterdir():
+            if display_and_check_files(fpath):
+                suggest_issue = True
+        return suggest_issue
+    if not path.is_file():
+        return suggest_issue
+    with io.open(path, "r", encoding="utf-8") as fobj:
+        for line in fobj:
+            if regex.search(line):
+                suggest_issue = True
+    display_summary(path)
+    return suggest_issue
+
+
 if __name__ == "__main__":
     init()
     display_repo_intro(pathlib.Path(sys.argv[1]))
